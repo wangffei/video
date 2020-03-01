4085ceed4b7214305c97bf4e3ddbb3df7adda390
wangffei <wf111444777@163.com>
2020-03-01 19:36:33 +0800

    gg

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::middleware('auth:api')->get('/user',function (Request $request) {
 	return $request->user();
 });
 
-Route::post('/uploadImage', "Api\EditerController@upload_img");
+Route::any('/uploadImage', "Api\EditerController@upload_img");
 Route::post('/submitMd', "Api\EditerController@submit");
 
 Route::get("/info", function (Request $request) {
