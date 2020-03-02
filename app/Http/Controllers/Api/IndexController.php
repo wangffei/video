cf5f4a0c4d8de85db18c614a6821a5d5bc53304f
= <2028777058@qq.com>
2020-03-02 16:23:50 +0800

    index ok

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -12,7 +12,8 @@
  	public function news($page , $limit){
  		$start = ($page - 1)*$limit ;
  		$list = DB::select("select news.heading , news.sub_heading , news.release_time , news.html_url as url , news.page_view , news.cover , news.author , tags.tag_name  from news , tags where tags.id = news.tag_id limit $start , $limit") ;
- 		$result = Array("code" => 200 , "msg" => "成功" , "data" => $list);
+ 		$count = DB::select("select count(*) as count from news")[0] -> count ;
+ 		$result = Array("code" => 200 , "msg" => "成功" , "data" => Array("list" => $list , "count"=> $count , "limit" => $limit ));
         return response(json_encode($result)) -> header("Content-Type", "application/json");
  	}
 
